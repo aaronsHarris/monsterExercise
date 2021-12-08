d03cbfab802fe5ca93b01e0f6b214d5e38f6d0d1
Aaron Harris <aaronsharris@gmail.com>
2021-12-08 11:35:46 -0600

    added search box omponent, added css for search box, imported it in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import { CardList } from "./components/card-list/CardList";
+import { SearchBox } from "./components/searchBox/SearchBox";
 class App extends Component {
   constructor() {
     super();
@@ -17,16 +18,16 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
   render() {
-    const { monsters, searchField } = this.state
-    const filteredMonsters = monsters.filter(monster =>
-     monster.name.toLowerCase().includes(searchField.toLowerCase()) 
-      )
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        <input
-          type="search"
-          placeholder="search monsters"
-          onChange={e =>  this.setState({ searchField: e.target.value }) }/>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={(e) => this.setState({ searchField: e.target.value })}
+        />
         <CardList monsters={filteredMonsters} />
       </div>
     );
